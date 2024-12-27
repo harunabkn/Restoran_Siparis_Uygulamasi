bec71d06b9bd1fea68e878413232352262796135
mirWalato <walatdundar@gmail.com>
2024-12-28 02:59:41 +0300

    POS formları eklendi

---
+++ Restoran_Siparis_Uygulamasi/Model/frmPOS.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Restoran_Siparis_Uygulamasi.Model
+{
+    public partial class frmPOS : Form
+    {
+        public frmPOS()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPOS_Load(object sender, EventArgs e)
+        {
+            guna2DataGridView1.BorderStyle = BorderStyle.FixedSingle;
+        }
+
+        private void lblMasa_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnCikis_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtAra_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Restoran_Siparis_Uygulamasi/Model/ucProduct.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Restoran_Siparis_Uygulamasi.Model
+{
+    partial class ucProduct
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ucProduct));
+            this.guna2ShadowPanel1 = new Guna.UI2.WinForms.Guna2ShadowPanel();
+            this.txtGorsel = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblIsim = new System.Windows.Forms.Label();
+            this.guna2Separator1 = new Guna.UI2.WinForms.Guna2Separator();
+            this.guna2ShadowPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtGorsel)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2ShadowPanel1
+            // 
+            this.guna2ShadowPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2ShadowPanel1.Controls.Add(this.guna2Separator1);
+            this.guna2ShadowPanel1.Controls.Add(this.panel1);
+            this.guna2ShadowPanel1.Controls.Add(this.txtGorsel);
+            this.guna2ShadowPanel1.FillColor = System.Drawing.Color.White;
+            this.guna2ShadowPanel1.Location = new System.Drawing.Point(3, 3);
+            this.guna2ShadowPanel1.Name = "guna2ShadowPanel1";
+            this.guna2ShadowPanel1.ShadowColor = System.Drawing.Color.Black;
+            this.guna2ShadowPanel1.Size = new System.Drawing.Size(187, 179);
+            this.guna2ShadowPanel1.TabIndex = 0;
+            // 
+            // txtGorsel
+            // 
+            this.txtGorsel.Image = ((System.Drawing.Image)(resources.GetObject("txtGorsel.Image")));
+            this.txtGorsel.ImageRotate = 0F;
+            this.txtGorsel.Location = new System.Drawing.Point(51, 17);
+            this.txtGorsel.Name = "txtGorsel";
+            this.txtGorsel.Size = new System.Drawing.Size(91, 89);
+            this.txtGorsel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.txtGorsel.TabIndex = 0;
+            this.txtGorsel.TabStop = false;
+            this.txtGorsel.UseTransparentBackground = true;
+            this.txtGorsel.Click += new System.EventHandler(this.txtGorsel_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblIsim);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 129);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(187, 50);
+            this.panel1.TabIndex = 1;
+            // 
+            // lblIsim
+            // 
+            this.lblIsim.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIsim.Location = new System.Drawing.Point(10, -13);
+            this.lblIsim.Name = "lblIsim";
+            this.lblIsim.Size = new System.Drawing.Size(168, 55);
+            this.lblIsim.TabIndex = 0;
+            this.lblIsim.Text = "Ürün İsmi";
+            this.lblIsim.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // guna2Separator1
+            // 
+            this.guna2Separator1.Location = new System.Drawing.Point(-6, 116);
+            this.guna2Separator1.Name = "guna2Separator1";
+            this.guna2Separator1.Size = new System.Drawing.Size(184, 10);
+            this.guna2Separator1.TabIndex = 2;
+            // 
+            // ucProduct
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.Controls.Add(this.guna2ShadowPanel1);
+            this.Name = "ucProduct";
+            this.Size = new System.Drawing.Size(193, 185);
+            this.guna2ShadowPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.txtGorsel)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2ShadowPanel guna2ShadowPanel1;
+        private Guna.UI2.WinForms.Guna2PictureBox txtGorsel;
+        private System.Windows.Forms.Panel panel1;
+        private Guna.UI2.WinForms.Guna2Separator guna2Separator1;
+        private System.Windows.Forms.Label lblIsim;
+    }
+}
